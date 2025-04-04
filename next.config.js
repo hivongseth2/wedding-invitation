819d9f82b25dba8328a85465c7a57e9257f00fd2
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2025-04-05 01:16:32 +0700

    fix build

--- next.config.js
+++ next.config.js
@@ -1,16 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['images.unsplash.com', 'via.placeholder.com'],
-    },
-    output: 'standalone',
-
-    reactStrictMode: true,
-    swcMinify: true,
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
+  images: {
+    domains: ['images.unsplash.com', 'via.placeholder.com'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config, { isServer }) => {
+    config.snapshot = {
+      ...config.snapshot,
+      managedPaths: [],
+    };
+    return config;
   }
-  
-  module.exports = nextConfig;
-  
+};
+
+module.exports = nextConfig;
